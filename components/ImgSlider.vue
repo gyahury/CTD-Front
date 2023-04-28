6cc52f2841cadb8bfd4fdc75fa6e508068f1e4a2
gyahury <rudgns1243@naver.com>
2023-04-28 23:49:34 +0900

    Chore: 동기화를 위한 슬라이더 삭제

--- components/ImgSlider.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <v-carousel hide-delimiters height="300">
-    <v-carousel-item src='/coach1.jpg' cover></v-carousel-item>
-
-    <v-carousel-item src='/coach1.jpg' cover></v-carousel-item>
-
-    <v-carousel-item src='/coach1.jpg' cover></v-carousel-item>
-  </v-carousel>
-</template>
